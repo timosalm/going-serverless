aeecd82d4454fa9259ecbf9f30c25768b7c22d00
Timo Salm <52704922+timosalm@users.noreply.github.com>
2024-03-13 06:48:07 +0100

    Improvements

---
+++ src/main/java/com/example/inclusion/InclusionApplication.java
@@ -0,0 +1,13 @@
+package com.example.inclusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InclusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inclusion/InclusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inclusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InclusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
