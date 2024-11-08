96b19a9781df091fa795abbb64433e35bfcdccb1
Timo Salm <accounts@timosalm.de>
2024-11-08 10:01:29 +0100

    Improvements

--- leyden/setup.sh
+++ leyden/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+# https://github.com/sdeleuze/spring-boot-leyden-demo/blob/main/image/setup.sh
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y --no-install-recommends tzdata ca-certificates git curl build-essential libfreetype6-dev libfontconfig-dev libcups2-dev libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libffi-dev autoconf file unzip zip nano
